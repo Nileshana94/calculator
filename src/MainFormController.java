415e75ab463dce13c9ced870f46e2e996ade37c0
OnFlov <nileshana94@gmail.com>
2022-12-02 09:33:23 +0530

    + Added methods for numbers to claculator

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -2,36 +2,46 @@ import javafx.scene.input.MouseEvent;
 
 public class MainFormController {
     public void numOneOnAction(MouseEvent mouseEvent) {
+        setNumber(1);
     }
 
     public void numTwoOnAction(MouseEvent mouseEvent) {
+        setNumber(2);
     }
 
     public void numThreeOnAction(MouseEvent mouseEvent) {
+        setNumber(3);
     }
 
     public void numFourOnAction(MouseEvent mouseEvent) {
+        setNumber(4);
     }
 
     public void numFiveOnAction(MouseEvent mouseEvent) {
+        setNumber(5);
     }
 
     public void numSixOnAction(MouseEvent mouseEvent) {
+        setNumber(6);
     }
 
     public void numSevenOnAction(MouseEvent mouseEvent) {
+        setNumber(7);
     }
 
     public void numEightOnAction(MouseEvent mouseEvent) {
+        setNumber(8);
     }
 
     public void numNineOnAction(MouseEvent mouseEvent) {
+        setNumber(9);
     }
 
     public void ClearOnAction(MouseEvent mouseEvent) {
     }
 
     public void NumZeroOnAction(MouseEvent mouseEvent) {
+        setNumber(0);
     }
 
     public void devOnAction(MouseEvent mouseEvent) {
@@ -47,5 +57,9 @@ public class MainFormController {
     }
 
     public void equalOnAction(MouseEvent mouseEvent) {
+    }
+
+    private void setNumber(double number){
+
     }
 }
