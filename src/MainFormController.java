a3bdcc936a8518dcc120e85fcedfdab2bdd45d15
OnFlov <nileshana94@gmail.com>
2022-12-02 09:17:40 +0530

    Initial commit

---
+++ src/MainFormController.java
@@ -0,0 +1,51 @@
+import javafx.scene.input.MouseEvent;
+
+public class MainFormController {
+    public void numOneOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numTwoOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numThreeOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numFourOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numFiveOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numSixOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numSevenOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numEightOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void numNineOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void ClearOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void NumZeroOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void devOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void plusOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void minusOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void multiplyOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void equalOnAction(MouseEvent mouseEvent) {
+    }
+}
